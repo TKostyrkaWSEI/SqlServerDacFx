0e4b1cc075c9e72fab8eaa42d0ced1d0aa46a747
Tomasz Kostyrka <TKostyrkaWSEI@users.noreply.github.com>
2019-05-19 13:15:07 +0200

    male zmiany

--- DBContributorsPack/TesterApp/Testers/Tester00.cs
+++ DBContributorsPack/TesterApp/Testers/Tester00.cs
@@ -9,6 +9,10 @@ namespace TesterApp.Testers
     {
         public void Run()
         {
+            //  trzeba znaleźć dobry MSBuild, ten, który używany jest przez VS/SSDT
+            //  i załączyć również biblioteki: Framework, Tasks.Core i Utilities.Core
+            //  C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
+
             string projectFileName = @"C:\GitHub\SqlServerDacFx\DBContributorsPack\TestDBSource\TestDBSource.sqlproj";
 
             ProjectCollection pc = new ProjectCollection();
@@ -22,8 +26,8 @@ namespace TesterApp.Testers
             BuildRequestData BuidlRequest = new BuildRequestData(projectFileName, GlobalProperty, null, new string[] { "Build" }, null);
             BuildResult buildResult = BuildManager.DefaultBuildManager.Build(new BuildParameters(pc), BuidlRequest);
 
-            Console.WriteLine(buildResult.Exception);
             Console.WriteLine(buildResult.OverallResult);
+            Console.WriteLine(buildResult.Exception);
             Console.WriteLine("------->");
             Console.ReadKey();
         }
